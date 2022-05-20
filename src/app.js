85014f9894f96cbcbea3152882eee7e64cec95d1
ronnyfm <ronnyfm@gmail.com>
2022-05-20 09:55:44 -0600

    module 2

--- src/app.js
+++ src/app.js
@@ -10,9 +10,52 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const accountData = fs.readFileSync(
+  path.join(__dirname, "json", "accounts.json"),
+  "utf8"
+);
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+  path.join(__dirname, "json", "users.json"),
+  "utf8"
+);
+const users = JSON.parse(userData);
+
 // index page
 app.get("/", (request, response) => {
-  response.render("index", {title: "Index"});
+  response.render("index", {
+    title: "Account Summary",
+    accounts: accounts,
+  });
+});
+
+// savings page
+app.get("/savings", (request, response) => {
+  response.render("account", {
+    account: accounts.savings,
+  });
+});
+
+// checking page
+app.get("/checking", (request, response) => {
+  response.render("account", {
+    account: accounts.checking,
+  });
+});
+
+// credit page
+app.get("/credit", (request, response) => {
+  response.render("account", {
+    account: accounts.credit,
+  });
+});
+
+// profile page
+app.get("/profile", (request, response) => {
+  response.render("profile", {
+    user: users[0],
+  });
 });
 
 app.listen(port, () => {
