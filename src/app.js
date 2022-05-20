ecb52cdb0614f7efe73ec39147d7dd952cee105a
ronnyfm <ronnyfm@gmail.com>
2022-05-20 10:29:28 -0600

    module 3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 const accountData = fs.readFileSync(
@@ -58,6 +59,52 @@ app.get("/profile", (request, response) => {
   });
 });
 
+// transfer page
+app.get("/transfer", (request, response) => {
+  response.render("transfer");
+});
+
+app.post("/transfer", (request, response) => {
+  const from = request.body.from;
+  const to = request.body.to;
+  const amount = request.body.amount;
+
+  accounts[from].balance -= amount;
+  accounts[to].balance += parseInt(amount, 10);
+
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join(__dirname, "json", "accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+
+  response.render("transfer", { message: "Transfer Completed" });
+});
+
+// payment page
+app.get("/payment", (request, response) => {
+  response.render("payment", {
+    account: accounts.credit,
+  });
+});
+
+app.post("/payment", (request, response) => {
+    const amount = request.body.amount;
+    
+    accounts.credit.balance -= amount;
+    accounts.credit.available += parseInt(amount, 10);
+    
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(
+        path.join(__dirname, "json", "accounts.json"),
+        accountsJSON,
+        "utf8"
+    );
+    
+    response.render("payment", { message: "Payment Successful", account: accounts.credit });
+});
+
 app.listen(port, () => {
-  console.log(`PS Project Running on port ${port}}!`);
+  console.log(`PS Project Running on port ${port}!`);
 });
